0922b31ec89fdb58d45e5188f0979df13840b951
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2022-12-10 21:07:54 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+
+import { Logon } from './pages/logon'
+import{GlobalStyle } from './styles/global'
+
+function App() {
+
+
+
+  return(
+
+
+    <>
+      <GlobalStyle/>
+        <Logon/>
+    </>
+    
+
+    
+      
+
+  )
+
+
+}
+
+export default App
---
+++ src/pages/logon/index.jsx
@@ -0,0 +1,67 @@
+
+import { MdEmail, MdLock, MdPerson } from 'react-icons/md'
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+
+
+import { useForm } from "react-hook-form";
+
+
+import { Container, Title, Column, TitleLogin, SubtitleLogin, CriarText, Row, Wrapper, TemConta } from './styles';
+
+const Logon = () => {
+
+    
+
+    const { control, handleSubmit, formState: { errors } } = useForm({
+        reValidateMode: 'onChange',
+        mode: 'onChange',
+    });
+
+    const onSubmit = async (formData) => {
+        try {
+            
+
+            
+        } catch (e) {
+            //TODO: HOUVE UM ERRO
+        }
+    };
+
+    console.log('errors', errors);
+
+    return (<>
+        <Header />
+        <Container>
+            <Column>
+                <Title>A plataforma para você aprender com experts, dominar as principais tecnologias
+                    e entrar mais rápido nas empresas mais desejadas.</Title>
+            </Column>
+            <Column>
+                <Wrapper>
+                    <TitleLogin>Comece agora grátis</TitleLogin>
+                    <SubtitleLogin>Crie sua conta e make the change._</SubtitleLogin>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                    <Input placeholder="Nome Completo" leftIcon={<MdPerson />} name="nome" control={control} />
+                        {errors.email && <span>Nome é obrigatório</span>}
+                        <Input placeholder="E-mail" leftIcon={<MdEmail />} name="email" control={control} />
+                        {errors.email && <span>E-mail é obrigatório</span>}
+                        <Input type="password" placeholder="Senha" leftIcon={<MdLock />} name="senha" control={control} />
+                        {errors.senha && <span>Senha é obrigatório</span>}
+                        <Button title="Fazer Login" variant="secondary" type="submit" />
+                    </form>
+                    <Row>
+                        <SubtitleLogin>Ao clicar em "criar minha conta grátis", declaro que aceito as Políticas de Privacidade e os Termos de Uso da DIO.</SubtitleLogin>
+                    </Row>
+                    <Row>
+                        <TemConta>Já tenho conta.</TemConta>
+                        <CriarText>Fazer Login</CriarText>
+                    </Row>
+                </Wrapper>
+            </Column>
+        </Container>
+    </>)
+}
+
+export { Logon }
